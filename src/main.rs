d31dfbfc212256d09a8d824e2d555be049b079b7
nate-nate-nate <natedunlap1@gmail.com>
2021-04-22 20:21:43 -0400

    console engine hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use console_engine::screen::Screen;
+use console_engine::pixel;
+
 fn main() {
-    println!("Hello, world!");
+    // create a screen of 20x11 characters
+    let mut scr = Screen::new(20,11);
+
+    // draw some shapes and prints some text
+    scr.rect(0,0, 19,10,pixel::pxl('#'));
+    scr.fill_circle(5,5, 3, pixel::pxl('*'));
+    scr.print(11,4, "Hello,");
+    scr.print(11,5, "World!");
+
+    // print the screen to the terminal
+    scr.draw();
 }
