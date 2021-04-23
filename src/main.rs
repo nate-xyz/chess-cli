51478c576823c140516a66dec32f15fae7e76287
nate-nate-nate <natedunlap1@gmail.com>
2021-04-22 21:33:36 -0400

    use engine instead of screen

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
-use console_engine::screen::Screen;
 use console_engine::pixel;
+use console_engine::Color;
+use console_engine::KeyCode;
 
 fn main() {
-    // create a screen of 20x11 characters
-    let mut scr = Screen::new(20,11);
+    // initializes a screen of 20x10 characters with a target of 3 frames per second
+    // coordinates will range from [0,0] to [19,9]
+    let mut engine = console_engine::ConsoleEngine::init(40, 20, 10);
+    let value = 14;
+    // main loop, be aware that you'll have to break it because ctrl+C is captured
+    loop {
+        engine.wait_frame(); // wait for next frame + capture inputs
+        engine.clear_screen(); // reset the screen
+    
+        engine.line(0, 0, 19, 9, pixel::pxl('#')); // draw a line of '#' from [0,0] to [19,9]
+        engine.print(0, 4, format!("Result: {}", value).as_str()); // prints some value at [0,4]
+    
+        engine.set_pxl(4, 0, pixel::pxl_fg('O', Color::Cyan)); // write a majestic cyan 'O' at [4,0]
 
-    // draw some shapes and prints some text
-    scr.rect(0,0, 19,10,pixel::pxl('#'));
-    scr.fill_circle(5,5, 3, pixel::pxl('*'));
-    scr.print(11,4, "Hello,");
-    scr.print(11,5, "World!");
-
-    // print the screen to the terminal
-    scr.draw();
+        if engine.is_key_pressed(KeyCode::Char('q')) { // if the user presses 'q' :
+            break; // exits app
+        }
+    
+        engine.draw(); // draw the screen
+    }
 }
