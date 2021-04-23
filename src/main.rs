606731f5867788dc0263471251ad3123e8015d7a
nate-nate-nate <natedunlap1@gmail.com>
2021-04-22 23:38:55 -0400

    change colors

--- src/main.rs
+++ src/main.rs
@@ -37,19 +37,18 @@ fn main() {
     let square = '█';
 
     //initialize engine
-    let mut engine = console_engine::ConsoleEngine::init(48, 48, 10);
+    let mut engine = console_engine::ConsoleEngine::init(48, 48, 30);
     //initialize board screen
     let mut board = Screen::new(24, 24);
 
     //draw board
     let gen_board: [i32; 8] = [0, 3, 6, 9, 12, 15, 18, 21];
     let dark_square = Color::Black;
-    let light_square = Color::Grey;
+    let light_square = Color::DarkGrey;
 
     for y in &gen_board {
         for x in &gen_board {
             let square_color = if *x % 2 == 0 { if *y % 2 == 0 {dark_square} else {light_square} } else  { if *y % 2 == 0 {light_square} else {dark_square} };
-
             board.fill_rect(*x, *y, *x+2, *y+2, pixel::pxl_fg(square, square_color));
         }
     }
