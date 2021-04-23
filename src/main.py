d5b3b944ed72bd2c79b49b6c95784eca6fcd85cc
ScuderiaHarrari <WIT4919@calu.edu>
2021-04-23 17:46:34 +0000

    we have two pieces now wow

--- src/main.py
+++ src/main.py
@@ -1,33 +1,27 @@
 from core import run_loop, Entity, style
 
 pieces = {
-    'R': '♜ ',
-    'N': '♞ ',
-    'B': '♗ ',
-    'Q': '♕ ',
-    'K': '♔ ',
-    'P': '♙ ',
-    'r': '♜ ',
-    'n': '♞ ',
-    'b': '♝ ',
-    'q': '♛ ',
-    'k': '♚ ',
-    'p': '♙ ',
+    'R': '♜',
+    'N': '♞',
+    'B': '♗',
+    'Q': '♕',
+    'K': '♔',
+    'P': '♙',
+    'r': '♜',
+    'n': '♞',
+    'b': '♝',
+    'q': '♛',
+    'k': '♚',
+    'p': '♙',
 
 }
 def game(state, press):
   mut_state = state
   if press == "q":
     mut_state["running"] = False
-  if press == "w":
-    mut_state["entities"]["player"].y -= 1
-  if press == "d":
-    mut_state["entities"]["player"].x += 1
-  if press == "s":
-    mut_state["entities"]["player"].y += 1
-  if press == "a":
-    mut_state["entities"]["player"].x -= 1
+
   return mut_state
 run_loop(game, {"running": True, "entities": {
-  "player": Entity(10, 10, "@", style.RED)
+  "Rook": Entity(40, 40, pieces['R'], style.RED),
+  "Knight": Entity(45, 40, pieces['N'], style.RED),
   }})
