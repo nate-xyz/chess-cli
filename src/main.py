690956cfe167aa18027a06c037088d21ef31bba2
nate-nate-nate <natedunlap1@gmail.com>
2021-04-27 14:24:25 -0400

    nicer squares

--- src/main.py
+++ src/main.py
@@ -60,12 +60,13 @@ entities = dict()
 
 
 def display_info(info_window):
+    #TODO: text wrapping to avoid errors when the terminal is too small
     global last_move_str
     global status_str
     global inputted_str
     global legal_move_str
     height, width = info_window.getmaxyx()\
-    
+
 
     info_window.attron(curses.color_pair(3))
     if board.turn == chess.WHITE:
@@ -79,7 +80,7 @@ def display_info(info_window):
 
     if status_str == "move is legal!":
         info_window.attron(curses.color_pair(8))
-    else: 
+    else:
         info_window.attron(curses.color_pair(9))
     info_window.addstr(3, 1, status_str)
     info_window.attroff(curses.color_pair(9))
@@ -87,7 +88,7 @@ def display_info(info_window):
     info_window.attron(curses.color_pair(8))
     info_window.addstr(5, 1, "{}: {}".format("legal moves",legal_move_str))
     info_window.attroff(curses.color_pair(9))
-    
+
     status_str = ""
 
 def update_input(prompt_window, key):
@@ -100,7 +101,7 @@ def update_input(prompt_window, key):
 
     prompt_window.addch(prompt_y_coord, prompt_x_coord, key)
     prompt_window.addch(prompt_y_coord, 0, '>')
-    
+
     prompt_x_coord += 1
 
     if prompt_x_coord >= width-1:
@@ -112,7 +113,7 @@ def update_input(prompt_window, key):
         prompt_y_coord = 1
         for i in range(1, height-1):
             prompt_window.addstr(i, prompt_x_coord, " " * (width-1))
-    
+
     if key==10: #enter key
         entered_move = True
         inputted_str = user_input_string
@@ -121,7 +122,7 @@ def update_input(prompt_window, key):
         prompt_x_coord = 1
         prompt_y_coord = 1
         prompt_window.addch(prompt_y_coord, 0, '>')
-        
+
         for i in range(1, height-1):
             prompt_window.addstr(i, prompt_x_coord, " " * (width-1))
     else:
@@ -154,8 +155,9 @@ def draw_board(board_window, board_FEN):
 
     for i in range(len(board_FEN)):
         current_piece = board_FEN[i]
-        
+
         if current_piece == '/':
+            board_window.addstr(y_coord, x_coord, chr(9))
             x_coord = og_xcoord
             y_coord += y_inc
             square_count += 1
@@ -168,7 +170,7 @@ def draw_board(board_window, board_FEN):
                 else:
                     color_pair = 5
                 board_window.attron(curses.color_pair(color_pair))
-                board_window.addch(y_coord, x_coord, ' ')
+                board_window.addstr(y_coord, x_coord, " "+chr(9))
                 board_window.attroff(curses.color_pair(color_pair))
                 square_count += 1
                 x_coord += x_inc
@@ -192,36 +194,36 @@ def draw_board(board_window, board_FEN):
                     color_pair = 6
                 else:
                     color_pair = 7
-            
+
             board_window.attron(curses.color_pair(color_pair))
-            #board_window.attron(curses.A_BOLD)
-        
-     
+            board_window.attron(curses.A_BOLD)
+
+
             #entities[color_str+rank[x_coord]+piece_name[start_board[i]]] = \
             #Entity(x_coord, y_coord, pieces[start_board[i]], piece_style)
 
-            board_window.addch(y_coord, x_coord, pieces[current_piece])
+            board_window.addstr(y_coord, x_coord, pieces[current_piece.upper()]+" ")
             #board_window.addch(y_coord, x_coord, 'x')
-            
-            
+
+
             board_window.attroff(curses.color_pair(color_pair))
-            #board_window.attroff(curses.A_BOLD)  
-           
-   
-                  
+            board_window.attroff(curses.A_BOLD)
+
+
+
             square_count += 1
             x_coord += x_inc
             continue
         else:
             print("error parsing starting FEN")
             break
-    
+
     for i in range(8):
         board_window.addch(og_ycoord-y_inc-1, og_xcoord+x_inc*i, x_notation_string[i])
         board_window.addch(og_ycoord+8*y_inc+1, og_xcoord+x_inc*i, x_notation_string[i])
         board_window.addch(og_ycoord+y_inc*i, og_xcoord-x_inc-1, y_notation_string[i])
         board_window.addch(og_ycoord+y_inc*i, og_xcoord+8*x_inc+1, y_notation_string[i])
-        
+
 
 def game_logic(board_window):
     global inputted_str
@@ -229,12 +231,12 @@ def game_logic(board_window):
     global status_str
     global entered_move
     global legal_move_str
-    global last_move_str 
+    global last_move_str
     inputted_str = inputted_str.strip(' ').strip('\0').strip('^@')
     legal_moves = []
- 
+
     #draw board
-    draw_board(board_window, board.board_fen())   
+    draw_board(board_window, board.board_fen())
 
     legal_move_str = ""
     for move in board.legal_moves:
@@ -244,7 +246,7 @@ def game_logic(board_window):
 
     if entered_move:
         entered_move = False
-        
+
 
         if inputted_str == 'undo':
             board.pop()
@@ -258,13 +260,13 @@ def game_logic(board_window):
                     curses.flash()
                     curses.beep()
                     board.push(chess.Move.from_uci(inputted_str))
-                    draw_board(board_window, board.board_fen())   
+                    draw_board(board_window, board.board_fen())
                     for move in board.legal_moves:
                         legal_moves.append(chess.Move.uci(move))
                         movo_str = chess.Move.uci(move) + " "
                         legal_move_str += movo_str
 
-    
+
 
 
 def draw_screen(stdscr):
@@ -273,7 +275,7 @@ def draw_screen(stdscr):
     cursor_y = 0
     stdscr = curses.initscr()
     height, width = stdscr.getmaxyx()
-    
+
     # Clear and refresh the screen for a blank canvas
     stdscr.clear()
     stdscr.refresh()
@@ -310,13 +312,13 @@ def draw_screen(stdscr):
     curses.init_pair(9, curses.COLOR_WHITE, curses.COLOR_RED)
 
     #start windows
-    
+
     board_window = curses.newwin(height-1, width//2, 0, 0)
     info_window = curses.newwin(height//2, width//2, 0, width//2)
     prompt_window = curses.newwin((height-1)//2, width//2, height//2, width//2)
 
     windows_array = [board_window, info_window, prompt_window]
-    
+
     # Loop where k is the last character pressed
     while (key != 15): # while not quitting
 
@@ -346,7 +348,7 @@ def draw_screen(stdscr):
             board_window.refresh()
             info_window.refresh()
             prompt_window.refresh()
-        
+
         #get winodw dimensions
         height, width = stdscr.getmaxyx()
         board_window_height, board_window_width = board_window.getmaxyx()
@@ -363,10 +365,15 @@ def draw_screen(stdscr):
         board_title = "board"[:width-1]
         info_title = "info"[:width-1]
         prompt_title = "prompt"[:width-1]
-        
+
         keystr = "Last key pressed: {}".format(key)[:width-1]
-        statusbarstr = "Press 'Ctrl+o' to exit | CHESS-CLI | Pos: {}, {}".format(cursor_x, cursor_y)+" | "+keystr+" | colors="+str(curses.COLORS)
-        
+        #statusbarstr = "Press 'Ctrl+o' to exit | CHESS-CLI | Pos: {}, {}".format(cursor_x, cursor_y)
+        statusbarstr = "Press 'Ctrl+o' to exit | CHESS-CLI"
+
+
+        statusbarfull = "{} | {}".format(statusbarstr, keystr)
+        #statusbarfull = ""
+
         if key == 0:
             keystr = "No key press detected..."[:width-1]
 
@@ -379,8 +386,8 @@ def draw_screen(stdscr):
         #render strings
         # Render status bar
         stdscr.attron(curses.color_pair(3))
-        stdscr.addstr(height-1, 0, statusbarstr)
-        stdscr.addstr(height-1, len(statusbarstr), " " * (width - len(statusbarstr) - 1))
+        stdscr.addstr(height-1, 0, statusbarfull)
+        stdscr.addstr(height-1, len(statusbarfull), " " * (width - len(statusbarfull) - 1))
         stdscr.attroff(curses.color_pair(3))
 
         # Turning on attributes for title
@@ -399,19 +406,17 @@ def draw_screen(stdscr):
             windows_array[i].attroff(curses.color_pair(2))
             windows_array[i].attroff(curses.A_BOLD)
 
-
         #external function calls
         update_input(prompt_window, key)
         game_logic(board_window)
         display_info(info_window)
-        
 
         # Refresh the screen
         stdscr.refresh()
         board_window.refresh()
         info_window.refresh()
         prompt_window.refresh()
-    
+
         # Wait for next input
         key = stdscr.getch()
 
