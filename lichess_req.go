e3607d5f221c75b5e26e002c34a2a0b1fe3011ac
nate-xyz <natedunlap1@gmail.com>
2022-01-02 16:58:50 -0500

    get full user profile

--- lichess_req.go
+++ lichess_req.go
@@ -9,7 +9,9 @@ import (
 
 var UserEmail string
 var Username string
+var UserProfile map[string]interface{}
 
+//get user email
 func GetEmail() error {
 	req, err := http.NewRequest("GET", fmt.Sprintf("%s/api/account/email", hostUrl), nil)
 	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", UserInfo.ApiToken))
@@ -41,6 +43,7 @@ func GetEmail() error {
 
 }
 
+//get username from profile json
 func GetUsername() error {
 
 	req, err := http.NewRequest("GET", fmt.Sprintf("%s/api/account", hostUrl), nil)
@@ -64,11 +67,36 @@ func GetUsername() error {
 		return err
 	}
 
-	// retrieve the access token out of the map, and return to caller
+	// retrieve the username out of the map, and return to caller
 	if !isNil(responseData["username"]) {
 		Username = responseData["username"].(string)
 		return nil
 	}
 	return fmt.Errorf("response interface is nil")
 
+}
+
+//get full user profile json
+func GetProfile() error {
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/api/account", hostUrl), nil)
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", UserInfo.ApiToken))
+	if err != nil {
+		return err
+	}
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	body, _ := io.ReadAll(resp.Body)
+	if resp.StatusCode != 200 {
+		return fmt.Errorf(string(body))
+	}
+	// unmarshal the json into a string map
+	err = json.Unmarshal(body, &UserProfile)
+	if err != nil {
+		return err
+	}
+	return nil
 }
