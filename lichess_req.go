e38b796323a26c383b15ebfa4433a0967f777d5e
nate-xyz <natedunlap1@gmail.com>
2022-01-02 17:28:04 -0500

    moved challenge request to lichess_req

--- lichess_req.go
+++ lichess_req.go
@@ -10,6 +10,7 @@ import (
 var UserEmail string
 var Username string
 var UserProfile map[string]interface{}
+var UserChallenges map[string]interface{}
 
 //get user email
 func GetEmail() error {
@@ -99,4 +100,40 @@ func GetProfile() error {
 		return err
 	}
 	return nil
+}
+
+func GetChallenges() {
+
+	//http GET returns array of objects(ChallengeJson) in and out
+	ChallengesArray := make([]string, 0)
+	var IncomingChallenges string = ""
+	var OutgoingChallenges string = ""
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/api/account/challenge", hostUrl), nil)
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", UserInfo.ApiToken))
+	if err != nil {
+		return
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	body, _ := io.ReadAll(resp.Body)
+
+	// unmarshal the json into a string map
+	var responseData map[string]interface{}
+	err = json.Unmarshal(body, &responseData)
+
+	// retrieve the access token out of the map, and return to caller
+	if !isNil(responseData["in"]) {
+
+		IncomingChallenges = responseData["in"].(string)
+		ChallengesArray = append(ChallengesArray, IncomingChallenges)
+	}
+
+	if !isNil(responseData["out"]) {
+
+		OutgoingChallenges = responseData["out"].(string)
+		ChallengesArray = append(ChallengesArray, OutgoingChallenges)
+	}
 }
