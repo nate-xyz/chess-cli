1b9c0b5b9305e67325a6c5bd1518ee2c7bd0a07c
hfwitte <harryfwitte@gmail.com>
2022-01-03 00:45:59 -0500

    offline and online friends

--- draw_screens.go
+++ draw_screens.go
@@ -176,7 +176,16 @@ func draw_lichess_challenges(screen *ncurses.Window) {
 
 	// Print rest of text
 	screen.MovePrint(start_y+1, start_x_subtitle, subtitle)
-	screen.MovePrint(start_y+3, (width/2)-2, "----")
+	for friend, status := range FriendsMap {
+
+		if status == false {
+			screen.MovePrint(start_y+3, (width/2)-2, "Friend: "+(friend)+"offline")
+		} else {
+			screen.MovePrint(start_y+3, (width/2)-2, "Friend: "+(friend)+"online")
+		}
+
+	}
+
 	for i, str := range additional_info {
 		screen.MovePrint(start_y+4+i, (width/2)-(len(str)/2), str)
 	}
